ed5df3e2f0bcc6182530d0eb39c1a54fd57373a5
alanpapillu <alan.papillu@hotmail.com>
2022-03-11 12:36:47 -0300

    cambios

--- sistema_ventas/login.php
+++ sistema_ventas/login.php
@@ -4,6 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 include_once("config.php");
+include_once("entidades/usuario.php");
 
 if($_POST){
   $usuario = trim($_REQUEST["txtUsuario"]);
